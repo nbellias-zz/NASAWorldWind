721160a61dee1f48b0b0b1b9e417219711f08760
Nikolaos Bellias <nikolaos.bellias@nibelsoft.com>
2021-12-15 09:15:59 +0200

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverallMapComponent } from './overall-map/overall-map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: OverallMapComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OverallMapComponent } from './overall-map/overall-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverallMapComponent
   ],
   imports: [
     BrowserModule,
